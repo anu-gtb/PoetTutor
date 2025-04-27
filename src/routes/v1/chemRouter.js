2402fea774c41e8208a5b78f3c72ccb39ab423d2
anu-gtb <140297541+anu-gtb@users.noreply.github.com>
2025-04-27 16:10:53 +0530

    Delete src/routes/v1 directory

--- src/routes/v1/chemRouter.js
+++
@@ -1,7 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const {chemController}=require('../../controllers');
-
-router.get('/',chemController.generate_poetry);
-
-module.exports=router;
